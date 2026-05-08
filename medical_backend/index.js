4ccc9b4d02b453a5944e662cfaf663fbd6975129
Dikshit09 <dikshitchoudhary09@gmail.com>
2026-05-08 22:49:30 +0530

    fix: cors updated for production

--- medical_backend/index.js
+++ medical_backend/index.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
   credentials: true
 }));
 app.use(cookieParser());
