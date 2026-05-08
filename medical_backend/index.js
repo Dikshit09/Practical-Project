47cc0df2c214305f711174fb1a5c06ef603c6c92
Dikshit09 <dikshitchoudhary09@gmail.com>
2026-05-09 00:06:49 +0530

    fix1

--- medical_backend/index.js
+++ medical_backend/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRoute from './routes/users.js';
 import authRoute from './routes/auth.js';
-import appointmentRoutes from './models/apointment.js';
+import appointmentRoutes from './routes/appointment.js';
 import doctorRoutes from './routes/doctor.js'
 import medicineScheduleRoutes from './routes/medicineSchedule.js'
 
